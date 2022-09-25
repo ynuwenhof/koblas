4e6ef22d46b23164906beb5c86214cf557bdd89d
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-09-25 11:20:13 +0200

    Add basic connection loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let listener = TcpListener::bind("").await?;
+
+    loop {
+        let (mut stream, _addr) = listener.accept().await?;
+
+        tokio::spawn(async move { stream.shutdown().await });
+    }
 }
