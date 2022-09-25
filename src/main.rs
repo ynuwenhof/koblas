d07c80aeb451d42999352cebbdc3d16119aa4823
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-09-25 15:21:25 +0200

    Add method selection message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod error;
-use tokio::net::TcpListener;
+
+use crate::error::Error;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
@@ -10,6 +13,27 @@ async fn main() -> color_eyre::Result<()> {
     loop {
         let (mut stream, _addr) = listener.accept().await?;
 
-        tokio::spawn(async move { stream.shutdown().await });
+        tokio::spawn(async move {
+            handle(&mut stream).await;
+            stream.shutdown().await
+        });
     }
+}
+
+const SOCKS_VERSION: u8 = 0x5;
+
+async fn handle(stream: &mut TcpStream) -> error::Result<()> {
+    let mut buf = [0u8; 2];
+    stream.read_exact(&mut buf).await?;
+
+    let version = buf[0];
+    if version != SOCKS_VERSION {
+        return Err(Error::InvalidVersion);
+    }
+
+    let len = buf[1] as usize;
+    let mut buf = vec![0u8; len];
+    stream.read_exact(&mut buf).await?;
+
+    Ok(())
 }
