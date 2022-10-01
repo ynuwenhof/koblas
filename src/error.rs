a8a7b233918dc8719160188916c8cec09ac93380
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-10-01 14:21:50 +0200

    Refactor error types and handling

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,87 @@
-use std::io::ErrorKind;
-use std::{io, result};
-use thiserror::Error;
+use itertools::Itertools;
+use std::fmt::{Display, Formatter};
+use std::net::AddrParseError;
+use std::string::FromUtf8Error;
+use std::{error, fmt, io, result};
 
 pub type Result<T> = result::Result<T, Error>;
 
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("invalid version")]
-    InvalidVersion,
-    #[error(transparent)]
-    Io(#[from] io::Error),
-    #[error(transparent)]
-    Request(#[from] RequestError),
+    InvalidVersion { expected: u8, found: u8 },
+    Io(io::Error),
+    Socks(SocksError),
 }
 
-#[derive(Error, Debug)]
-pub enum RequestError {
-    #[error("general error")]
-    General = 0x1,
-    #[error("network unreachable")]
-    NetworkUnreachable = 0x3,
-    #[error("host unreachable")]
-    HostUnreachable,
-    #[error("connection refused")]
-    ConnectionRefused,
-    #[error("unsupported command")]
-    UnsupportedCommand = 0x7,
-    #[error("unsupported address")]
-    UnsupportedAddress,
-}
+impl error::Error for Error {}
 
-impl From<io::Error> for RequestError {
-    fn from(err: io::Error) -> Self {
-        match err.kind() {
-            ErrorKind::NetworkUnreachable => Self::NetworkUnreachable,
-            ErrorKind::HostUnreachable => Self::HostUnreachable,
-            ErrorKind::ConnectionRefused => Self::ConnectionRefused,
-            _ => Self::General,
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidVersion { expected, found } => {
+                write!(f, "invalid version (expected {expected}, found {found})")
+            }
+            Self::Io(err) => err.fmt(f),
+            Error::Socks(err) => err.fmt(f),
         }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<SocksError> for Error {
+    fn from(err: SocksError) -> Self {
+        Self::Socks(err)
+    }
+}
+
+#[derive(Debug)]
+pub enum SocksError {
+    Addr(AddrParseError),
+    InvalidAddr { expected: Vec<u8>, found: u8 },
+    InvalidCommand { expected: u8, found: u8 },
+    Io(io::Error),
+    Utf8(FromUtf8Error),
+}
+
+impl error::Error for SocksError {}
+
+impl Display for SocksError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Addr(err) => err.fmt(f),
+            Self::InvalidAddr { expected, found } => {
+                let expected = expected.iter().join(", ");
+
+                write!(f, "invalid addr (expected {expected}, found {found})")
+            }
+            Self::InvalidCommand { expected, found } => {
+                write!(f, "invalid command (expected {expected}, found {found})")
+            }
+            Self::Io(err) => err.fmt(f),
+            Self::Utf8(err) => err.fmt(f),
+        }
+    }
+}
+
+impl From<AddrParseError> for SocksError {
+    fn from(err: AddrParseError) -> Self {
+        Self::Addr(err)
+    }
+}
+
+impl From<io::Error> for SocksError {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<FromUtf8Error> for SocksError {
+    fn from(err: FromUtf8Error) -> Self {
+        Self::Utf8(err)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-#![feature(io_error_more)]
-
 mod error;
 
-use crate::error::{Error, RequestError};
+use crate::error::{Error, SocksError};
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use sqlx::SqlitePool;
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
@@ -41,13 +39,16 @@ const SOCKS_VERSION: u8 = 0x5;
 const SUCCESS_REPLY: u8 = 0x0;
 const CONNECT_COMMAND: u8 = 0x1;
 
-async fn handle(stream: &mut TcpStream, pool: SqlitePool) -> error::Result<()> {
+async fn handle(stream: &mut TcpStream, _pool: SqlitePool) -> error::Result<()> {
     let mut buf = [0u8; 2];
     stream.read_exact(&mut buf).await?;
 
-    let version = buf[0];
-    if version != SOCKS_VERSION {
-        return Err(Error::InvalidVersion);
+    let ver = buf[0];
+    if ver != SOCKS_VERSION {
+        return Err(Error::InvalidVersion {
+            expected: SOCKS_VERSION,
+            found: ver,
+        });
     }
 
     let len = buf[1] as usize;
@@ -59,14 +60,45 @@ async fn handle(stream: &mut TcpStream, pool: SqlitePool) -> error::Result<()> {
     let mut buf = [0u8; 4];
     stream.read_exact(&mut buf).await?;
 
-    let version = buf[0];
-    if version != SOCKS_VERSION {
-        return Err(Error::InvalidVersion);
+    let ver = buf[0];
+    if ver != SOCKS_VERSION {
+        return Err(Error::InvalidVersion {
+            expected: SOCKS_VERSION,
+            found: ver,
+        });
     }
 
+    let mut peer = match socks(stream, buf).await {
+        Ok(peer) => peer,
+        Err(err) => {
+            let reply = match err {
+                SocksError::InvalidAddr { .. } => 0x8,
+                SocksError::InvalidCommand { .. } => 0x7,
+                _ => 0x1,
+            };
+
+            let buf = [SOCKS_VERSION, reply, 0, IPV4_TYPE, 0, 0, 0, 0, 0, 0];
+            stream.write_all(&buf).await?;
+
+            return Err(err.into());
+        }
+    };
+
+    let buf = [SOCKS_VERSION, SUCCESS_REPLY, 0, IPV4_TYPE, 0, 0, 0, 0, 0, 0];
+    stream.write_all(&buf).await?;
+
+    io::copy_bidirectional(stream, &mut peer).await?;
+
+    Ok(())
+}
+
+async fn socks(stream: &mut TcpStream, buf: [u8; 4]) -> Result<TcpStream, SocksError> {
     let cmd = buf[1];
     if cmd != CONNECT_COMMAND {
-        return Err(RequestError::UnsupportedCommand.into());
+        return Err(SocksError::InvalidCommand {
+            expected: CONNECT_COMMAND,
+            found: cmd,
+        });
     }
 
     let addr = buf[3];
@@ -76,39 +108,32 @@ async fn handle(stream: &mut TcpStream, pool: SqlitePool) -> error::Result<()> {
             stream.read_exact(&mut octets).await?;
 
             let port = stream.read_u16().await?;
-
             SocketAddr::new(Ipv4Addr::from(octets).into(), port)
         }
         DOMAIN_TYPE => {
             let len = stream.read_u8().await? as usize;
-
             let mut buf = vec![0u8; len];
             stream.read_exact(&mut buf).await?;
 
-            let domain = String::from_utf8(buf).map_err(|_| RequestError::UnsupportedAddress)?;
-
+            let domain = String::from_utf8(buf)?;
             let port = stream.read_u16().await?;
 
-            SocketAddr::from_str(&format!("{domain}:{port}"))
-                .map_err(|_| RequestError::UnsupportedAddress)?
+            SocketAddr::from_str(&format!("{domain}:{port}"))?
         }
         IPV6_TYPE => {
             let mut octets = [0u8; 16];
             stream.read_exact(&mut octets).await?;
 
             let port = stream.read_u16().await?;
-
             SocketAddr::new(Ipv6Addr::from(octets).into(), port)
         }
-        _ => return Err(RequestError::UnsupportedAddress.into()),
+        _ => {
+            return Err(SocksError::InvalidAddr {
+                expected: vec![IPV4_TYPE, DOMAIN_TYPE, IPV6_TYPE],
+                found: addr,
+            })
+        }
     };
 
-    let mut peer = TcpStream::connect(dest).await.map_err(RequestError::from)?;
-
-    let buf = [SOCKS_VERSION, SUCCESS_REPLY, 0, IPV4_TYPE, 0, 0, 0, 0, 0, 0];
-    stream.write_all(&buf).await?;
-
-    io::copy_bidirectional(stream, &mut peer).await?;
-
-    Ok(())
+    Ok(TcpStream::connect(dest).await?)
 }
