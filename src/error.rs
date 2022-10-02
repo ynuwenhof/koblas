bb9fe60f4a4a6470c63a9820fb15a33256b28a4e
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-10-02 12:45:45 +0200

    Add authentication error type

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use argon2::password_hash;
 use itertools::Itertools;
 use std::fmt::{Display, Formatter};
 use std::net::AddrParseError;
@@ -8,6 +9,7 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    Auth(AuthError),
     InvalidVersion { expected: u8, found: u8 },
     Io(io::Error),
     Socks(SocksError),
@@ -18,12 +20,19 @@ impl error::Error for Error {}
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
+            Self::Auth(err) => err.fmt(f),
             Self::InvalidVersion { expected, found } => {
                 write!(f, "invalid version (expected {expected}, found {found})")
             }
             Self::Io(err) => err.fmt(f),
-            Error::Socks(err) => err.fmt(f),
+            Self::Socks(err) => err.fmt(f),
         }
+    }
+}
+
+impl From<AuthError> for Error {
+    fn from(err: AuthError) -> Self {
+        Self::Auth(err)
     }
 }
 
@@ -36,6 +45,47 @@ impl From<io::Error> for Error {
 impl From<SocksError> for Error {
     fn from(err: SocksError) -> Self {
         Self::Socks(err)
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    InvalidVersion { expected: u8, found: u8 },
+    Io(io::Error),
+    Password(password_hash::Error),
+    Utf8(FromUtf8Error),
+}
+
+impl error::Error for AuthError {}
+
+impl Display for AuthError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidVersion { expected, found } => {
+                write!(f, "invalid version (expected {expected}, found {found})")
+            }
+            Self::Io(err) => err.fmt(f),
+            Self::Password(err) => err.fmt(f),
+            Self::Utf8(err) => err.fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for AuthError {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<password_hash::Error> for AuthError {
+    fn from(err: password_hash::Error) -> Self {
+        Self::Password(err)
+    }
+}
+
+impl From<FromUtf8Error> for AuthError {
+    fn from(err: FromUtf8Error) -> Self {
+        Self::Utf8(err)
     }
 }
 
