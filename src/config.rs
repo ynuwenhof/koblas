b8d0a337f422f0197a7b42156778d7d2b9831fd6
Yannic Nuwenhof <ynuwenhof@pm.me>
2023-01-15 21:06:19 +0100

    Update command line arguments

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,12 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
-use std::fmt;
-use std::fmt::{Debug, Formatter};
 use std::fs;
-use std::net::{IpAddr, SocketAddr};
 use std::path::Path;
 use toml::de;
 
 #[derive(Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
-    #[serde(default)]
-    pub server: Server,
     #[serde(default)]
     pub users: BTreeMap<String, String>,
 }
@@ -27,37 +22,4 @@ impl Config {
     fn from_slice(bytes: &[u8]) -> Result<Self, de::Error> {
         toml::from_slice(bytes)
     }
-}
-
-impl Debug for Config {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Config")
-            .field("server", &self.server)
-            .finish()
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(deny_unknown_fields)]
-pub struct Server {
-    #[serde(default = "default_socket_addr")]
-    pub addr: SocketAddr,
-    #[serde(default)]
-    pub auth: bool,
-    #[serde(default)]
-    pub anon: bool,
-}
-
-impl Default for Server {
-    fn default() -> Self {
-        Self {
-            addr: default_socket_addr(),
-            auth: Default::default(),
-            anon: Default::default(),
-        }
-    }
-}
-
-fn default_socket_addr() -> SocketAddr {
-    SocketAddr::new(IpAddr::from([127, 0, 0, 1]), 1080)
 }
